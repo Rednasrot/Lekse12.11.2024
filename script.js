9ee4a09008e98e200c64b57cc90662d574162d08
Rednasrot <rednasrot@gmail.com>
2024-11-15 13:58:16 +0100

    Oppdatering v1.5

--- script.js
+++ script.js
@@ -1,1 +1,76 @@
 console.log('Hello!');
+
+
+//variabler:
+
+const lives = 5;
+
+let currentlife = lives + 1;
+
+lives ++
+
+//litenbokstavCamelCase
+
+console.log(lives);
+console.log(currentlife);
+
+//prmitive datatyper:
+let string = "4 5 6 7";
+newString = string + 1;
+
+console.log (string);
+console.log(newString);
+
+let calculation = 4 + 6;
+console.log(calculation)
+
+let boolean = true;
+console.log(boolean);
+if passwordIsTypedTooFast => ("Incorrect Password")
+
+//ikke primitive datatyper: function, array, object
+
+let myFunc =
+
+let myArray = [
+    "mandag", "tirsdag", "onsdag"
+
+]
+
+console.log(myArray);
+console.log(myArray[0])
+
+//Assignment operators
+
+let x = 0;
+
+x += 4;
+x -= 5;
+x *= 2; 
+x %= 0.5;
+
+console.log(x);
+
+//comparison operators
+
+let a= 10;
+let b= "10";
+let c= 5;
+
+console.log(a == b);
+console.log(a === b);
+console.log(a != c);
+console.log(a > c);
+console.log(a <= a);
+
+
+
+//logical operators && || !
+
+let a = true
+let b = false
+
+
+console.log(a && b);
+console.log(a || b);
+
